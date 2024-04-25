e390cce804dd58de2c960744ad09346baa98394b
张丹 <zh880517@163.com>
2024-04-25 22:43:21 +0800

    Simulate（未完）

---
+++ Editor/Simulate/SimulateObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace FrameLine
+{
+    public class SimulateObject : ScriptableObject
+    {
+        [SerializeField]
+        protected FrameLineSimulate Owner;
+        [SerializeField]
+        internal string ClipGUID;
+
+        public virtual void OnReset()
+        {
+
+        }
+    }
+}
